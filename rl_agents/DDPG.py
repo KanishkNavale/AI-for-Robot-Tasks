21711b0e03b5afb6ef7a3fb2f7264379c6b1d8aa
KanishkNavale <navalekanishk@gmail.com>
2022-01-04 16:39:55 +0100

    PyTorch OLGA_FIX

--- rl_agents/DDPG.py
+++ rl_agents/DDPG.py
@@ -1,11 +1,16 @@
 # Library Imports
 import numpy as np
-import tensorflow as tf
+import torch as T
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
 from replay_buffers.PER import PrioritizedReplayBuffer
 from replay_buffers.utils import LinearSchedule
-import copy
-tf.random.set_seed(0)
+T.manual_seed(0)
 np.random.seed(0)
+
+
+device = T.device("cuda:0" if T.cuda.is_available() else "cpu")
 
 
 class OUNoise:
@@ -28,95 +33,109 @@ class OUNoise:
         return self.state * self.scale
 
 
-class Critic(tf.keras.Model):
+class Critic(nn.Module):
     """Defines a Critic Deep Learning Network"""
 
-    def __init__(self, density=512, name='critic'):
+    def __init__(self, input_dim, beta, density=512, name='critic'):
         super(Critic, self).__init__()
 
         self.model_name = name
-        self.checkpoint = self.model_name + '.h5'
+        self.checkpoint = self.model_name
 
-        self.H1 = tf.keras.layers.Dense(density, activation='relu')
-        self.H2 = tf.keras.layers.Dense(density, activation='relu')
-        self.drop = tf.keras.layers.Dropout(0.1)
-        self.H3 = tf.keras.layers.Dense(density, activation='relu')
-        self.H4 = tf.keras.layers.Dense(density, activation='relu')
-        self.Q = tf.keras.layers.Dense(1, activation=None)
+        self.H1 = nn.Linear(input_dim, density)
+        self.H2 = nn.Linear(density, density)
+        self.drop = nn.Dropout(p=0.1)
+        self.H3 = nn.Linear(density, density)
+        self.H4 = nn.Linear(density, density)
+        self.Q = nn.Linear(density, 1)
 
-    @tf.function()
-    def call(self, state, action):
-        state = tf.cast(state, tf.float64)
-        action = tf.cast(action, tf.float64)
-        action = self.H1(tf.concat([state, action], axis=1))
-        action = self.H2(action)
-        action = self.drop(action)
-        action = self.H3(action)
-        action = self.H4(action)
-        Q = self.Q(action)
-        return Q
+        self.optimizer = optim.Adam(self.parameters(), lr=beta)
+        self.to(device)
+
+    def forward(self, state, action):
+        value = T.hstack((state, action))
+        value = F.relu(self.H1(value))
+        value = F.relu(self.H2(value))
+        value = self.drop(value)
+        value = F.relu(self.H3(value))
+        value = F.relu(self.H4(value))
+        value = self.Q(value)
+        return value
+
+    def save_model(self, path):
+        T.save(self.state_dict(), path + self.checkpoint)
+
+    def load_model(self, path):
+        self.load_state_dict(T.load(path + self.checkpoint))
 
 
-class Actor(tf.keras.Model):
+class Actor(nn.Module):
     """Defines a Actor Deep Learning Network"""
 
-    def __init__(self, n_actions, density=512, name='actor'):
+    def __init__(self, input_dim, n_actions, alpha, density=512, name='actor'):
         super(Actor, self).__init__()
 
         self.model_name = name
-        self.checkpoint = self.model_name + '.h5'
+        self.checkpoint = self.model_name
 
-        self.H1 = tf.keras.layers.Dense(density, activation='relu')
-        self.H2 = tf.keras.layers.Dense(density, activation='relu')
-        self.drop = tf.keras.layers.Dropout(0.1)
-        self.H3 = tf.keras.layers.Dense(density, activation='relu')
-        self.H4 = tf.keras.layers.Dense(density, activation='relu')
-        self.mu = tf.keras.layers.Dense(n_actions, activation='tanh')
+        self.H1 = nn.Linear(input_dim, density)
+        self.H2 = nn.Linear(density, density)
+        self.drop = nn.Dropout(p=0.1)
+        self.H3 = nn.Linear(density, density)
+        self.H4 = nn.Linear(density, density)
+        self.mu = nn.Linear(density, n_actions)
 
-    @tf.function()
-    def call(self, state):
-        state = self.H1(state)
-        state = self.H2(state)
-        state = self.drop(state)
-        state = self.H3(state)
-        state = self.H4(state)
-        mu = self.mu(state)
-        return mu
+        self.optimizer = optim.Adam(self.parameters(), lr=alpha)
+        self.to(device)
+
+    def forward(self, state):
+        action = F.relu(self.H1(state))
+        action = F.relu(self.H2(action))
+        action = self.drop(action)
+        action = F.relu(self.H3(action))
+        action = F.relu(self.H4(action))
+        action = T.tanh(self.mu(action))
+        return action
+
+    def save_model(self, path):
+        T.save(self.state_dict(), path + self.checkpoint)
+
+    def load_model(self, path):
+        self.load_state_dict(T.load(path + self.checkpoint))
 
 
 class Agent:
     def __init__(self, env, datapath, n_games, alpha=0.0001,
                  beta=0.001, gamma=0.99, tau=0.005, batch_size=64,
-                 noise='normal', per_alpha=0.6, per_beta=0.4,
-                 use_her=False):
+                 noise='normal', per_alpha=0.6, per_beta=0.4, enable_HER=False):
 
         self.env = env
-        self.gamma = tf.convert_to_tensor([gamma], dtype=tf.float32)
-        self.tau = tf.convert_to_tensor([tau], dtype=tf.float32)
+        self.gamma = T.tensor(gamma, dtype=T.float32).to(device)
+        self.tau = T.tensor(tau, dtype=T.float32).to(device)
         self.n_actions = env.action_space.shape[0]
         self.obs_shape = env.observation_space.shape[0]
         self.datapath = datapath
         self.n_games = n_games
         self.optim_steps = 0
-        self.max_size = 2500000
+        self.max_size = 25000
         self.memory = PrioritizedReplayBuffer(self.max_size, per_alpha)
-        self.beta_scheduler = LinearSchedule(n_games, per_beta, 0.99)
+        self.beta_scheduler = LinearSchedule(n_games, per_beta, 0.7)
+        self.her = enable_HER
 
         self.batch_size = batch_size
         self.noise = noise
-        self.max_action = env.action_space.high
-        self.min_action = env.action_space.low
-        self.her = use_her
+        self.max_action = T.tensor(
+            env.action_space.high, dtype=T.float32).to(device)
+        self.min_action = T.tensor(
+            env.action_space.low, dtype=T.float32).to(device)
 
-        self.actor = Actor(self.n_actions, name='actor')
-        self.critic = Critic(name='critic')
-        self.target_actor = Actor(self.n_actions, name='target_actor')
-        self.target_critic = Critic(name='target_critic')
-
-        self.actor.compile(tf.keras.optimizers.Adam(alpha))
-        self.critic.compile(tf.keras.optimizers.Adam(beta))
-        self.target_actor.compile(tf.keras.optimizers.Adam(alpha))
-        self.target_critic.compile(tf.keras.optimizers.Adam(beta))
+        self.actor = Actor(self.obs_shape, self.n_actions, alpha, name='actor')
+        self.critic = Critic(
+            self.obs_shape + self.n_actions, beta, name='critic')
+        self.target_actor = Actor(
+            self.obs_shape, self.n_actions, alpha, name='target_actor')
+        self.target_critic = Critic(
+            self.obs_shape + self.n_actions, beta, name='target_critic')
 
         if self.noise == 'normal':
             self.noise_param = 0.001
@@ -129,42 +148,35 @@ class Agent:
             self.scalar = 0.001
             self.scalar_decay = 0.01
             self.desired_distance = 0.01
-            self.noisy_actor = Actor(self.n_actions, name='noisy_actor')
-
-            # Fire-up 'noisy_actor' to set params.
-            obs = env.observation_space.sample()
-            state = tf.convert_to_tensor([obs], dtype=tf.float64)
-            self.noisy_actor(state)
+            self.noisy_actor = Actor(
+                self.obs_shape, self.n_actions, alpha, name='noisy_actor')
 
         self.update_networks()
-
-    def update_noisy_actor(self):
-        weights = []
-        for weight in self.actor.weights:
-            noise = tf.random.normal(
-                shape=weight.shape, stddev=self.scalar)
-            weights.append(weight + noise)
-        self.noisy_actor.set_weights(weights)
 
     def update_networks(self):
         tau = self.tau
 
-        weights = []
-        targets = self.target_actor.weights
-        for i, weight in enumerate(self.actor.weights):
-            weights.append(weight * tau + targets[i] * (1 - tau))
-        self.target_actor.set_weights(weights)
+        for critic_weights, target_critic_weights in zip(self.critic.parameters(), self.target_critic.parameters()):
+            target_critic_weights.data.copy_(
+                tau * critic_weights.data + (1 - tau) * target_critic_weights.data)
 
-        weights = []
-        targets = self.target_critic.weights
-        for i, weight in enumerate(self.critic.weights):
-            weights.append(weight * tau + targets[i] * (1 - tau))
-        self.target_critic.set_weights(weights)
+        for actor_weights, target_actor_weights in zip(self.actor.parameters(), self.target_actor.parameters()):
+            target_actor_weights.data.copy_(
+                tau * actor_weights.data + (1 - tau) * target_actor_weights.data)
+
+    def update_noisy_actor(self):
+        with T.no_grad():
+            for actor_weights, noisy_actor_weights in zip(self.actor.parameters(), self.noisy_actor.parameters()):
+                noise = np.random.uniform(
+                    0.0, self.scalar, actor_weights.shape)
+                noise = T.tensor(noise, dtype=T.float32).to(
+                    actor_weights.device)
+                noisy_actor_weights.data.copy_(actor_weights.data + noise)
 
     def store(self, states, actions, rewards, next_states, dones, targets):
         if self.her:
             # Hyperparameter for Future Goal Sampling
-            k = 8
+            k = 4
 
             # Augment the replay buffer
             T = len(actions)
@@ -200,36 +212,23 @@ class Agent:
                 new_state = np.concatenate((new_state, target))
                 self.memory.add(state, action, reward, new_state, done)
 
-    def save_models(self):
-        self.actor.save_weights(self.datapath + self.actor.checkpoint)
-        self.critic.save_weights(self.datapath + self.critic.checkpoint)
-        self.target_actor.save_weights(self.datapath +
-                                       self.target_actor.checkpoint)
-        self.target_critic.save_weights(self.datapath +
-                                        self.target_critic.checkpoint)
-
-    def load_models(self):
-        self.actor.load_weights(self.datapath + self.actor.checkpoint)
-        self.critic.load_weights(self.datapath + self.critic.checkpoint)
-        self.target_actor.load_weights(self.datapath +
-                                       self.target_actor.checkpoint)
-        self.target_critic.load_weights(self.datapath +
-                                        self.target_critic.checkpoint)
-
     def choose_action(self, observation):
-        state = tf.convert_to_tensor([observation], dtype=tf.float64)
-        action = self.actor(state)
+        self.actor.eval()
+        state = T.tensor(observation, dtype=T.float32).to(device)
+        action = self.actor.forward(state)
 
         if self.noise == 'normal':
-            action += tf.random.normal([self.n_actions], 0.0, self.noise_param)
+            noise = np.random.uniform(0.0, self.noise_param, action.shape[0])
+            action += T.tensor(noise, dtype=T.float32).to(device)
 
         elif self.noise == 'ou':
-            action += self.noise()
+            action += T.tensor(self.noise(), dtype=T.float32).to(device)
 
         elif self.noise == 'param':
             self.update_noisy_actor()
+            self.noisy_actor.eval()
             action_noised = self.noisy_actor(state)
-            distance = tf.linalg.norm(action - action_noised)
+            distance = T.linalg.norm(action - action_noised)
             self.distances.append(distance)
             if distance > self.desired_distance:
                 self.scalar *= self.scalar_decay
@@ -237,8 +236,23 @@ class Agent:
                 self.scalar /= self.scalar_decay
             action += action_noised
 
-        # action = tf.clip_by_value(action, self.min_action, self.max_action)
-        return action[0].numpy()
+        action = T.clamp(action, self.min_action, self.max_action)
+        self.actor.train()
+        return action.detach().cpu().numpy()
+
+    def save_models(self):
+        self.actor.save_model(self.datapath)
+        self.actor.save_model(self.datapath)
+        self.target_actor.save_model(self.datapath)
+        self.critic.save_model(self.datapath)
+        self.target_critic.save_model(self.datapath)
+
+    def load_models(self):
+        self.actor.load_model(self.datapath)
+        self.actor.load_model(self.datapath)
+        self.target_actor.load_model(self.datapath)
+        self.critic.load_model(self.datapath)
+        self.target_critic.load_model(self.datapath)
 
     def optimize(self):
         if len(self.memory._storage) < self.batch_size:
@@ -248,46 +262,40 @@ class Agent:
         state, action, reward, new_state, done, weights, indices = self.memory.sample(
             self.batch_size, beta)
 
-        state = tf.convert_to_tensor(np.vstack(state), dtype=tf.float64)
-        action = tf.convert_to_tensor(np.vstack(action), dtype=tf.float64)
-        done = tf.convert_to_tensor(np.vstack(1 - done), dtype=tf.float32)
-        reward = tf.convert_to_tensor(np.vstack(reward), dtype=tf.float32)
-        weights = tf.convert_to_tensor(np.vstack(weights), dtype=tf.float32)
-        new_state = tf.convert_to_tensor(
-            np.vstack(new_state), dtype=tf.float64)
+        state = T.tensor(np.vstack(state), dtype=T.float32).to(device)
+        action = T.tensor(np.vstack(action), dtype=T.float32).to(device)
+        done = T.tensor(np.vstack(1 - done), dtype=T.float32).to(device)
+        reward = T.tensor(np.vstack(reward), dtype=T.float32).to(device)
+        new_state = T.tensor(np.vstack(new_state), dtype=T.float32).to(device)
+        weights = T.tensor(np.vstack(weights), dtype=T.float32).to(device)
 
-        with tf.GradientTape() as tape:
-            # Compute the Q value estimate of the target network
-            Q_target = self.target_critic(
-                new_state, self.target_actor(new_state))
-            # Compute Y
-            Y = reward + (done * [self.gamma] * Q_target)
-            # Compute Q value estimate of critic
-            Q = self.critic(state, action)
-            # Calculate TD errors
-            TD_errors = (Y - Q)
-            # Weight TD errors
-            weighted_TD_errors = tf.math.sqrt(weights) * TD_errors
-            # Create a zero tensor
-            zero_tensor = tf.zeros(weighted_TD_errors.shape)
-            # Compute critic loss, MSE of weighted TD_r
-            critic_loss = tf.keras.losses.mse(weighted_TD_errors, zero_tensor)
+        self.target_actor.eval()
+        self.target_critic.eval()
+        self.critic.eval()
 
-        critic_network_gradient = tape.gradient(
-            critic_loss, self.critic.trainable_variables)
-        self.critic.optimizer.apply_gradients(zip(
-            critic_network_gradient, self.critic.trainable_variables))
+        Q_target = self.target_critic(new_state, self.target_actor(new_state))
+        Y = reward + (done * self.gamma * Q_target)
+        Q = self.critic(state, action)
+        TD_errors = T.sub(Y, Q)
 
-        with tf.GradientTape() as tape:
-            actor_loss = - \
-                tf.math.reduce_mean(self.critic(state, self.actor(state)))
+        # Weight TD errors
+        weighted_TD_errors = T.mul(TD_errors, weights)
+        zero_tensor = T.zeros_like(
+            weighted_TD_errors, dtype=T.float32).to(device)
 
-        actor_network_gradient = tape.gradient(
-            actor_loss, self.actor.trainable_variables)
-        self.actor.optimizer.apply_gradients(zip(
-            actor_network_gradient, self.actor.trainable_variables))
+        # Compute & Update Critic losses
+        critic_loss = F.mse_loss(weighted_TD_errors, zero_tensor).to(device)
+        self.critic.optimizer.zero_grad()
+        critic_loss.backward()
+        self.critic.optimizer.step()
 
-        td_errors = TD_errors.numpy()
+        # Compute & Update Actor losses
+        actor_loss = -self.critic(state, self.actor(state)).mean()
+        self.actor.optimizer.zero_grad()
+        actor_loss.backward()
+        self.actor.optimizer.step()
+
+        td_errors = TD_errors.detach().cpu().numpy()
         new_priorities = np.abs(td_errors) + 1e-6
         self.memory.update_priorities(indices, new_priorities)
 
